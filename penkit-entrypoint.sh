a147b5a7f7d472e61f4714addff50bd3699fd267
Don Humphreys <dhumphreys88@gmail.com>
2017-03-16 23:44:27 -0400

    Build and push test docker images for branches

--- penkit-entrypoint.sh
+++ penkit-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # if running as root, fix docker.sock permissions
-if [ "$(id -u)" = "0" ]; then
+if [ "$(id -u)" = "0" ] && [ -f /var/run/docker.sock ] ; then
   addgroup -g $(stat -c "%g" /var/run/docker.sock) docker
   addgroup ruby docker
 fi
